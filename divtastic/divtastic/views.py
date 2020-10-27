ee4ce41ee5ccb19da8c05b0fec15778d64cee17f
DJ Kemmet <kemmetdaniel@gmail.com>
2020-10-26 19:19:46 -0500

    Templates implemented.

--- divtastic/divtastic/views.py
+++ divtastic/divtastic/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def divtastic_landing(request):
-    return HttpResponse("<h1> You've Reached the home page</h1>")
+    return render(request, 'root/landing.html',)
--- divtastic/research/views.py
+++ divtastic/research/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def symbol_research(request):
-    return HttpResponse("<h1>Lets investigate a stock!")
+    return render(request, 'research/new_stock_profile.html', {"data":"No Data"})
     
 def redirect_app_root(request):
     return HttpResponseRedirect('/')
