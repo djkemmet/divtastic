e0e673541955d3bcddc05d290cbb9d6c2667b406
DJ Kemmet <kemmetdaniel@gmail.com>
2020-10-29 22:54:12 -0500

    SO MUCH PROGRESS
    Got data from Dividend.com into the template, added action buttons to the bottom of the page. Made navigation responsive to login/out
    Sign up button alerts to status of development

--- divtastic/research/views.py
+++ divtastic/research/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .forms import symbol_lookup_form
 from research.functions import *
 import yfinance as yf
+from lxml import etree
+import requests
 
 # Create your views here.
 def symbol_research(request):
@@ -14,25 +16,32 @@ def symbol_research(request):
 
     # If the form is good, go get all the data.
     if form.is_valid():
-        form.save(commit=True)
+        
         data = dict()
         data['symbol'] = request.POST['symbol'].upper()
 
+        # yfinance Data
         symbol = yf.Ticker(request.POST['symbol'])
         data['yfinance_data'] = symbol.info
 
-        # These Come from dividend.com page scrape
-        data['company_name'] = 'Source From Dividend.com'
+        # Get the ACTUAL LINK (aka handle their redirect...)
+        url1 = 'https://dividend.com/search?q=' + request.POST['symbol']
+        response1 = requests.get(url1, allow_redirects=True)
+
+        url = response1.url
+        response = requests.get(url)
+
+        html = response.text
+        dom = etree.HTML(html)
+
         data['tax_type'] = 'Source From Dividend.com'
-        data['declaration_date'] = "Find it"
-
-
-
-        data['pte_ratio'] = 'Find it.'
-        data['payout_ratio'] = 'Find it.'
-        data['payout_date'] = 'Find it.'
-        data['record_date'] = 'Find it.'
-        data['dividend_yeild'] = 'Find it.'
+        data['frequency'] = dom.xpath('/html/body/main/section/section[1]/div/div/div/article/div[2]/div[3]/div[1]/section/div[2]/div/div[1]/div[5]/div[2]/text()')[0]
+        data['consecutive_increase'] = dom.xpath('/html/body/main/section/section[1]/div/div/div/article/div[2]/div[3]/div[1]/section/div[2]/div/div[1]/div[4]/div[2]/text()')[0]
+        data['payout_ratio'] = dom.xpath('/html/body/main/section/section[1]/div/div/div/article/div[2]/div[3]/div[1]/section/div[2]/div/div[1]/div[3]/div[2]/text()')[0]
+        data['next_payout'] = dom.xpath('/html/body/main/section/section[1]/div/div/div/article/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/text()')[0]
+        data['next_payout_date'] = dom.xpath('/html/body/main/section/section[1]/div/div/div/article/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]/text()')[0]
+        data['tax_mode'] = dom.xpath('/html/body/main/section/section[1]/div/div/div/article/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[3]/div[2]/text()')[0]
+        data['latest_quote'] = dom.xpath('/html/body/main/div[5]/div[1]/section/div/div[2]/div[2]/div[1]/div[2]/div[2]/text()')[0].strip('\n')
 
         # Return a profile_results.html template here with the data returned from, 
         # this function i guess?
